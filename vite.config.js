ea2238ef30422fc39e362c060095a0a3b1d9cf49
diljith krishna <diljithkrishna56@gmail.com>
2024-07-08 21:05:54 +0530

    proxy issue solved

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.BACKEND_URL,
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
+})
+
+
 
